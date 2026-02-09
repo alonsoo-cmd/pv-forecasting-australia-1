6e8c58f1b82914d0f3485f008048d4a8cd11c93a
alonsoo-cmd <alongutisan1@gmail.com>
2026-02-09 12:43:27 +0100

    cambios graficas

--- utils/graph_pipeline.py
+++ utils/graph_pipeline.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 def plot_continuous_horizon0(
     y_true,
     y_pred,
-    start_idx=1680,
+    start_idx=2520,
     n_days=7,
     title=None
 ):
@@ -14,6 +14,7 @@ def plot_continuous_horizon0(
     """
     y_true = np.asarray(y_true)
     y_pred = np.asarray(y_pred)
+    print("Checking commit")
 
     if y_true.ndim == 2:
         y_true = y_true[:, 0]
